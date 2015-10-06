8ba2249f8da96574c7987bbb0478f22e48762dc8
Gilles Degols <gdegols224@gmail.com>
2015-10-06 00:41:49 -0700

    Basic variants.highlander_search

--- apps/variants/src/variants/api.py
+++ apps/variants/src/variants/api.py
@@ -208,6 +208,8 @@ class VariantDetail(APIView):
         # Returns some information on a specific variant
         if pk == 'search':
             return self.search(request=request)
+        elif pk == 'highlander_search':
+            return self.highlander_search(request=request)
 
         if len(pk) == 0:
             return Response(json.dumps({'status':-1,'error':'Variant id not given.'}))
@@ -222,6 +224,8 @@ class VariantDetail(APIView):
         # Create a new variant
         if data == 'search':
             return self.search(request)
+        elif data == 'highlander_search':
+            return self.highlander_search(request)
 
         status = -1
 
@@ -359,6 +363,144 @@ class VariantDetail(APIView):
 
                 # We save the modified variant
                 good_variants[interesting_rowkey].append(mapped_variant)
+
+            # We have the different variants correctly separated
+            for rowkey_part in good_variants:
+
+                # We generate the data for the variant (we give multiple rows but they are all related to the same variant)
+                current_variant = VariantSerializer(request=request, pk=good_variants[rowkey_part][0]['pk'], impala_data=good_variants[rowkey_part])
+
+                # We store the variant (only the data, not the object)
+                if len(result_set) < number_of_good_variants:
+                    result_set.append(current_variant.data)
+                else: # The last row  is used to check if there are still variants to list
+                    last_pk = good_variants[rowkey_part][len(good_variants[rowkey_part])-1]['id']
+            db.close(handle)
+
+        # We format the results and send them back
+        result['variants'] = result_set
+        result['nextPageToken'] = last_pk
+
+        return Response(result)
+
+    def highlander_search(self, request):
+        """
+        Realize an advanced search on the impala data. We receive a query made by Highlander
+        so the fields might be very specific.
+        :param request:
+        :return:
+        """
+
+        result = {'status':1,'text':'Everything is alright.'}
+        data = request.data
+
+        """ For test only """
+        if len(data) == 0:
+            data = {
+                "method": "SELECT",
+                "fields": "count(*)",# list of fields separated like a sql query, by a comma
+                "condition": "variants.referenceName = '1'", # list of conditions (WHERE clause) like a sql query
+                "limit": 5000,
+                "offset": 0,
+                "order-by": ""
+            }
+
+        """ End test """
+
+        # TODO add verification for the code we receive
+
+        # We get the keys associated to the json we received
+        fc = formatConverters(input_file='stuff.vcf',output_file='stuff.json')
+        mapping = fc.getMappingJsonToParquet()
+
+        # We prepare the query
+        query_data = {}
+        query_data['method'] = data['method']
+        if query_data['method'] != 'SELECT':
+            query_data['method'] = 'SELECT'
+
+        fields = data['fields']
+        for json_parameter in mapping:
+            fields = fields.replace(json_parameter, mapping[json_parameter])
+        query_data['fields'] = fields
+
+        condition = data['condition']
+        for json_parameter in mapping:
+            condition = condition.replace(json_parameter, mapping[json_parameter])
+        if len(condition) > 0:
+            query_data['condition'] = "WHERE "+condition
+        else:
+            query_data['condition'] = ""
+
+        if data['limit'] > 0:
+            query_data['limit'] = "LIMIT "+str(data['limit'])
+        else:
+            query_data['limit'] = ""
+
+        if data['offset'] > 0:
+            query_data['offset'] = "OFFSET "+str(data['offset'])
+        else:
+            query_data['offset'] = ""
+
+        order_by = data['order-by']
+        for json_parameter in mapping:
+            order_by = order_by.replace(json_parameter, mapping[json_parameter])
+        query_data['order-by'] = order_by
+
+        # TODO format the query when it involves specific samples (a little bit tricky to do that...)!
+
+        # We format the final query (which might be invalid but we don't care right now)
+        query = query_data['method']+" "+query_data['fields']+" FROM variants "+query_data['condition']+" "+query_data['limit']+" "+query_data['offset']
+
+        f = open('superhello-queries.txt','a')
+        f.write(query+'\n')
+        f.close()
+
+        # We execute the query on parquet
+        query_server = get_query_server_config(name='impala')
+        db = dbms.get(request.user, query_server=query_server)
+        try:
+            handle = db.execute_and_wait(hql_query(query), timeout_sec=360.0)
+        except Exception as e:
+            result['status'] = 0
+            result['text'] = 'Impossible to execute the query.'
+            result['detail'] = str(e)
+            return Response(result)
+
+        result_set = []
+        last_pk = ''
+        if handle:
+            raw_data = db.fetch(handle, rows=1000000 + 1) # + 1 as we want to know there are multiple pages
+            columns = raw_data.cols()
+
+            # We need to separate the different variants based on the rowkeys
+            good_variants = {}
+            number_of_good_variants = 0
+            ints = []
+            for raw_variant in raw_data.rows():
+                if isinstance(raw_variant[0], (int,long)): # we made a select count(*) so we only got a number, not multiple variants
+                    ints.append(raw_variant[0])
+                    continue
+
+                interesting_rowkey = raw_variant[0].split('|')
+                interesting_rowkey.pop()
+                interesting_rowkey = '|'.join(interesting_rowkey)+'|'
+                if interesting_rowkey not in good_variants:
+                    good_variants[interesting_rowkey] = []
+                    number_of_good_variants += 1
+
+                # We map the column names and the list of data for the current row
+                mapped_variant = {}
+                for i in xrange(len(columns)):
+                    mapped_variant[columns[i]] = raw_variant[i]
+
+                # We save the modified variant
+                good_variants[interesting_rowkey].append(mapped_variant)
+
+            # If we have a list of ints, we return the result
+            if len(ints) > 0:
+                result['values'] = ints
+                return Response(result)
 
             # We have the different variants correctly separated
             for rowkey_part in good_variants:
