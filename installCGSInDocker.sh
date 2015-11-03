586071d485c9f3f8948628f882ca65705d4fd6e7
Gilles Degols <gilles@degols.net>
2015-11-03 15:34:24 -0500

    Fix dependency problem (django rest framework)

--- installCGSInDocker.sh
+++ installCGSInDocker.sh
@@ -7,11 +7,11 @@ echo '> Install modules for the variants app'
 sudo easy_install pip
 sudo pip install ordereddict
 sudo pip install counter
-sudo yum install -y git
+#sudo yum install -y git
 # git clone https://github.com/jamescasbon/PyVCF
 # sudo python PyVCF/setup.py install
 sudo pip install pyvcf
-sudo pip install djangorestframework
+sudo pip install djangorestframework==3.2.5
 sudo pip install markdown
 sudo pip install django-filter
 
